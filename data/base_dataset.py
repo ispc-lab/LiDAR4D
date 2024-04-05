6a91bff59b526198e9e8f25be140026c5d9eb891
dyfcalid <18850942930@163.com>
2024-04-05 20:40:08 +0800

    fix dependency

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -1,8 +1,8 @@
 import numpy as np
 import torch
-import trimesh
 from packaging import version as pver
 from dataclasses import dataclass
+# import trimesh
 
 
 def custom_meshgrid(*args):
@@ -102,43 +102,43 @@ def get_lidar_rays(poses, intrinsics, H, W, N=-1, patch_size=1):
     return results
 
 
-def visualize_poses(poses, size=0.1):
-    # poses: [B, 4, 4]
+# def visualize_poses(poses, size=0.1):
+#     # poses: [B, 4, 4]
 
-    axes = trimesh.creation.axis(axis_length=4)
-    box = trimesh.primitives.Box(extents=(2, 2, 2)).as_outline()
-    box.colors = np.array([[128, 128, 128]] * len(box.entities))
-    objects = [axes, box]
+#     axes = trimesh.creation.axis(axis_length=4)
+#     box = trimesh.primitives.Box(extents=(2, 2, 2)).as_outline()
+#     box.colors = np.array([[128, 128, 128]] * len(box.entities))
+#     objects = [axes, box]
 
-    for pose in poses:
-        # a camera is visualized with 8 line segments.
-        pos = pose[:3, 3]
-        a = pos + size * pose[:3, 0] + size * pose[:3, 1] + size * pose[:3, 2]
-        b = pos - size * pose[:3, 0] + size * pose[:3, 1] + size * pose[:3, 2]
-        c = pos - size * pose[:3, 0] - size * pose[:3, 1] + size * pose[:3, 2]
-        d = pos + size * pose[:3, 0] - size * pose[:3, 1] + size * pose[:3, 2]
+#     for pose in poses:
+#         # a camera is visualized with 8 line segments.
+#         pos = pose[:3, 3]
+#         a = pos + size * pose[:3, 0] + size * pose[:3, 1] + size * pose[:3, 2]
+#         b = pos - size * pose[:3, 0] + size * pose[:3, 1] + size * pose[:3, 2]
+#         c = pos - size * pose[:3, 0] - size * pose[:3, 1] + size * pose[:3, 2]
+#         d = pos + size * pose[:3, 0] - size * pose[:3, 1] + size * pose[:3, 2]
 
-        dir = (a + b + c + d) / 4 - pos
-        dir = dir / (np.linalg.norm(dir) + 1e-8)
-        o = pos + dir * 3
+#         dir = (a + b + c + d) / 4 - pos
+#         dir = dir / (np.linalg.norm(dir) + 1e-8)
+#         o = pos + dir * 3
 
-        segs = np.array(
-            [
-                [pos, a],
-                [pos, b],
-                [pos, c],
-                [pos, d],
-                [a, b],
-                [b, c],
-                [c, d],
-                [d, a],
-                [pos, o],
-            ]
-        )
-        segs = trimesh.load_path(segs)
-        objects.append(segs)
+#         segs = np.array(
+#             [
+#                 [pos, a],
+#                 [pos, b],
+#                 [pos, c],
+#                 [pos, d],
+#                 [a, b],
+#                 [b, c],
+#                 [c, d],
+#                 [d, a],
+#                 [pos, o],
+#             ]
+#         )
+#         segs = trimesh.load_path(segs)
+#         objects.append(segs)
 
-    trimesh.Scene(objects).show()
+#     trimesh.Scene(objects).show()
 
 
 @dataclass
